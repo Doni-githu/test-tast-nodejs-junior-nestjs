416ab01904216694dda0ee4f226306230d1dc11f
Doniyor <ddonierov96@gmail.com>
2023-11-25 05:30:54 +0500

    add global db url

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { ContactsModule } from './contacts/contacts.module';
 import { MongooseModule } from "@nestjs/mongoose"
 
 @Module({
-  imports: [ContactsModule, MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [ContactsModule, MongooseModule.forRoot('mongodb+srv://doni:4jMbpjwz76AQUDxt@cluster2.y0ompzd.mongodb.net/?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
